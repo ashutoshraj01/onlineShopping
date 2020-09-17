8dff9dd47e99a2b0412859d93e480591558a1e5b
Ashutosh Raj <ashutoshraj908@gmail.com>
2020-09-17 18:33:43 +0530

    working on signIn process using JWT

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../Models/user');
 const { errorHandler } = require('../Helpers/dbErrorHandler');
-
+const jwt = require('jsonwebtoken');  // to generate signed token
+const expressJwt = require('express-jwt'); // for authorization check
 
 exports.signup = (req,res) =>{
    console.log("req.body--->",req.body);
@@ -16,4 +17,20 @@ exports.signup = (req,res) =>{
          return res.json({user});
    });
 
-};  
+};
+
+exports.signIn = (req, res) => {
+    // find user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) =>{
+        // user not found
+        if(err  || !user){
+            return res.status(400).json({
+                err:'User with email doesn\'t exist. Please signup!'
+            })
+        }
+        // user found
+        // make sure email and password are matched!
+        
+    });
+}
--- Routes/user.js
+++ Routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require("../Controllers/user");
+const { signup, signIn } = require("../Controllers/user");
 const { userSignupValidator } = require("../Validators/index"); // middleware
 
 router.post("/signup",userSignupValidator,signup); // first req will be validated, then moved forward
+router.post("/signin",signIn);
 
 module.exports = router;
