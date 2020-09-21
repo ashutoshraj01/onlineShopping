7a471c2723351770bfef685eefe4f0e0bec78672
Ashutosh Raj <ashutoshraj908@gmail.com>
2020-09-22 00:16:45 +0530

    model for category

---
+++ Models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+    },
+   
+},
+ {
+     timestamp: true,
+ }
+);
+
+
+module.exports = mongoose.model("Category",categorySchema);
