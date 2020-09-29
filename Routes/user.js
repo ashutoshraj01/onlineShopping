d7aa803efbe8e5b8c8fab61a218221cb2ca23b06
Ashutosh Raj <ashutoshraj908@gmail.com>
2020-09-29 20:58:39 +0530

    added routes for user

--- Routes/user.js
+++ Routes/user.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../Controllers/auth");
-const { findUserById } = require("../Controllers/user");
+const { findUserById, read, update } = require("../Controllers/user");
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) =>{
         res.json({ user: req.profile})
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
+
+
 router.param('userId',findUserById)
 
 module.exports = router;
