9f32a3e4c56a92c793db34ee793fddb511ebd161
Ashutosh Raj <ashutoshraj908@gmail.com>
2020-09-30 10:13:33 +0530

    refactoring

--- Controllers/user.js
+++ Controllers/user.js
@@ -11,3 +11,14 @@ exports.findUserById = (req,res,next,id) =>{
         next();
     })
 }
+
+exports.viewUserProfile = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt =undefined;
+  return res.json(req.profile);
+}
+
+
+exports.updateUserProfile = (req, res) => {
+    
+}
--- Routes/user.js
+++ Routes/user.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../Controllers/auth");
-const { findUserById, read, update } = require("../Controllers/user");
+const { findUserById, viewUserProfile, updateUserProfile } = require("../Controllers/user");
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) =>{
         res.json({ user: req.profile})
 });
 
-router.get('/user/:userId', requireSignin, isAuth, read);
-router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/user/:userId', requireSignin, isAuth, viewUserProfile);
+router.put('/user/:userId', requireSignin, isAuth, updateUserProfile);
 
 
 router.param('userId',findUserById)
